f32f564f380f81b92314075f733d5a658f5bfd05
aleong1 <aleong1@stuy.edu>
2018-10-19 20:11:13 -0400

    faster now

--- prewordladder/neighbors.py
+++ prewordladder/neighbors.py
@@ -5,37 +5,31 @@ fin = open('dictall.txt','r')
 finLines = fin.read().split('\n')
 
 inputFile = open(sys.argv[1], 'r')
-inputLines = inputFile.read().split('\n')
+inputWords = inputFile.read().strip().split('\n')
 inputFile.close()
-
 outputFile = open(sys.argv[2], 'w')
 
+alphabet = list(map(chr, range(97, 123))) #list of all lowercase letters
+pastLengths = list()
 dictionary = dict()
-lenWords = list()
 
-def simLength(input):
-    length = len(input)
-    for word in finLines:  #for each word in long list of words
-        if len(word) == length:  #for same length words
-            makeNeighbors(word)
+for original in inputWords:   #words in inputFile
+    wordLength = len(original)
+    if wordLength not in pastLengths:   #if this is a new length word
+        pastLengths.append(wordLength)
+        sameLength = [x for x in finLines if len(x) == wordLength]
+        setWords = set(sameLength)
+        for word in sameLength:
+            neighbors = list()
+            for character in range(len(word)):
+                for letter in alphabet:
+                    if letter != word[character]:
+                        newWord = word[:character] + letter + word[character + 1:]
+                        if newWord in setWords:
+                            neighbors.append(newWord)
+                dictionary[word] = neighbors
 
-def makeNeighbors(key):
-    neighbors = list()
-    for word in finLines:  #for each word in long list of words
-        if len(word) == len(key):  #same length
-            similar = 0
-            for character in range(len(key)):  #character in the word from inputFile
-                if key[character] == word[character]:
-                    similar += 1
-            if similar == len(word) - 1:
-                neighbors.append(word)
-    dictionary[key] = neighbors
-
-for line in inputLines:    #for each word in inputFile
-    if not dict() or len(line) not in lenWords:  #if dictionary is empty or word is a new length
-        simLength(line)
-        lenWords.append(len(line))   #put already used lengths in a list
-    outputFile.write(line + "," + str(len(dictionary[line])) + '\n')
+    outputFile.write(original + "," + str(len(dictionary[original])) + '\n')
 
 fin.close()
 outputFile.close()
