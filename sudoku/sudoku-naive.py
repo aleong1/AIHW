1f8f65134b3157084102a2b83355072d1be371bf
Alexia Leong <aleong1@stuy.edu>
2018-11-09 14:52:24 -0500

    idk

--- sudoku/sudoku-naive.py
+++ sudoku/sudoku-naive.py
@@ -1,3 +1,12 @@
 #! /usr/bin/python3
 import sys
 
+allBoards = open(sys.argv[1], "r")
+boards = allBoards.strip().split()
+outputFile = open(sys.argv[2], "w")
+name = sys.argv[3]
+
+board = list()
+
+for a in boards:
+    board.append(a.split("\n")
